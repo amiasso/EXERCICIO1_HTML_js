0e6ebf6c8be1e1e3bf9bc0d840071d1fefe0e1aa
amiasso <amiasso@msn.com>
2023-01-18 09:12:11 -0300

    add_code

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 
 const form = document.getElementById('comparar');
 
-function validar(numeroA){
-    var numero = comparar.numeroA.value;
-    var repetirNumero = comparar.numeroB.value;
+function validar(){
+var valida = parseInt(validar)
+var numero = comparar.numeroA.value;
+var repetirNumero = comparar.numeroB.value;
 
-    if(numero === "" ||repetirNumero <= 5){
-        alert("coloque um numero maior que 5");
-    comparar.repetirNumero.focus();
-    return false;
-    }else {
-        alert("seu numero é valido");
-    }
-    
+if(numero > 5){
+    alert(" No campo A, coloque um numero menor ou igual a 5");
+        comparar.repetirNumero.focus();
+}else if(repetirNumero > 10){
+    alert("No campo B, digite um numero entre 5 e 10");
+}else{
+    alert("seu numero é valido");
+}
 }
     
     
